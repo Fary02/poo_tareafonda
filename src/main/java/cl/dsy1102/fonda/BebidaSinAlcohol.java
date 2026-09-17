d7852f3e44b96cba35a6cc94a699490e61c1b74a
Nythra02 <alamf01@gmail.com>
2026-09-17 12:52:06 -0300

    Se crea clase hija "BebidaSinAlcohol" y se reformatea

---
+++ src/main/java/cl/dsy1102/fonda/BebidaSinAlcohol.java
@@ -0,0 +1,54 @@
+package cl.dsy1102.fonda;
+
+// Creacion de clase hija con herencia / extends y super de clase padre
+
+public class BebidaSinAlcohol extends Bebida {
+
+  // Unico atributo unico de la clase hija
+
+  public int azucarPorLitro;
+
+  // Constructor con atributos de la clase padre y super
+
+  public BebidaSinAlcohol(String nombre, int volumenML, int stock, double precio, String detalle, int azucarPorLitro) {
+
+    super(nombre, volumenML, stock, precio, detalle);
+
+    this.azucarPorLitro = azucarPorLitro;
+
+  }
+
+  // GETTERS Y SETTERS
+
+
+  public int getAzucarPorLitro() {
+
+    return azucarPorLitro;
+
+  }
+
+  public void setAzucarPorlitro(int azucarPorLitro) {
+
+    this.azucarPorLitro = azucarPorLitro;
+
+  }
+
+  // Methods de clase padre con @OVERRIDE
+
+  @Override
+
+  public void calcularPrecio() {
+
+    System.out.println("El precio de la bebida sin alcohol es de: " + precio);
+
+  }
+
+  @Override
+
+  public void obtenerDetalle() {
+
+    System.out.println("La bebida sin alcohol: " + nombre + " tiene un valor de: " + precio + " y existe: " + stock + " unidades disponibles");
+
+  }
+
+}
