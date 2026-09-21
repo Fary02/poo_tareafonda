be7b0643d36c933d9f09935c0efa9622bf31bba3
Nythra02 <alamf01@gmail.com>
2026-09-21 17:07:57 -0300

    Se instancias los objetos de forma correcta, falta integracion los methods de interface

--- src/main/java/cl/dsy1102/fonda/Main.java
+++ src/main/java/cl/dsy1102/fonda/Main.java
@@ -16,15 +16,14 @@ public class Main {
         // TODO 4: solicitar las cuatro ventas indicadas en el enunciado.
         // TODO 5: buscar por nombre "Chicha" y listar todas las bebidas.
 
-        // Se instancia cada objeto solicitado
+        // Se instancia cada objeto solicitado de forma correcta
 
-        BebidaAlcoholica chicha = new BebidaAlcoholica("Chicha", 1000, 40, 5000, "una rica chica a base de uva", 2, 12.0, false, true);
-        BebidaAlcoholica piscoSour = new BebidaAlcoholica("Pisco Sour", 500, 25, 2500, "un rico pisco sour", 2, 18.0, true, false);
-        BebidaSinAlcohol chichaSinAlcohol = new BebidaSinAlcohol("Chicha sin alchohol", 1000, 60, 1500, "chica en su version para niños", 95);
-        BebidaSinAlcohol moteConHuesillo = new BebidaSinAlcohol("Mote con huesillo", 400, 50, 10000, "Lo mejor de la fonda, este mote con huesillo justifica su inversion", 70);
+        Bebida chicha = new BebidaAlcoholica("Chicha", 1000, 40, 5000, "una rica chica a base de uva", 2, 12.0, false, true);
+        Bebida piscoSour = new BebidaAlcoholica("Pisco Sour", 500, 25, 2500, "un rico pisco sour", 2, 18.0, true, false);
+        Bebida chichaSinAlcohol = new BebidaSinAlcohol("Chicha sin alchohol", 1000, 60, 1500, "chica en su version para niños", 95);
+        Bebida moteConHuesillo = new BebidaSinAlcohol("Mote con huesillo", 400, 50, 10000, "Lo mejor de la fonda, este mote con huesillo justifica su inversion", 70);
 
         // Se ocupa el method solicitado y por mi lado coloque otros methods para los demas objetos
-
         chicha.restringirVenta();
 
         piscoSour.obtenerDetalle();
