4ad19e186c09133bb05a69fcddf5273aee7e11bb
Fary02 <fa.alam@duocuc.cl>
2026-09-24 18:39:17 -0300

    Se agrega el ultimo CRUD faltante

--- src/main/java/cl/dsy1102/fonda/GestorFonda.java
+++ src/main/java/cl/dsy1102/fonda/GestorFonda.java
@@ -9,55 +9,74 @@ import java.util.List;
 
 public class GestorFonda {
 
-  // Se importa la clase PADRE Bebida como atributo
+    // Se importa la clase PADRE Bebida como atributo
 
-  public List<Bebida> bebidas;
+    public List<Bebida> bebidas;
 
-  // Constuctor method con method List
+    // Constuctor method con method List
 
-  public GestorFonda() {
+    public GestorFonda() {
 
-    this.bebidas = new ArrayList<>();
+        this.bebidas = new ArrayList<>();
 
-  }
+    }
 
 // Registrar / Create
 
-  public void registrarBebida(Bebida bebida) {
+    public void registrarBebida(Bebida bebida) {
 
-    bebidas.add(bebida);
+        bebidas.add(bebida);
 
-  }
+        // Se llama al metodo GET para obtener solo el nombre (de lo contrario, lanza todos los valores declarados en toString de la clase PADRE)
 
-  // Buscar / Read
+        System.out.println("Se registro la bebida: " + bebida.getNombre());
 
-  public List<Bebida> buscarPorNombre(String nombre) {
-
-    ArrayList<Bebida> nombreBebida = new ArrayList<>();
-    for (Bebida bebida : bebidas) {
-
-      if (bebida.getNombre().equalsIgnoreCase(nombre)) {
-
-        nombreBebida.add(bebida);
-
-      }
     }
-    return nombreBebida;
-  }
 
-  // Vender / UPDATE (FALTA INCORPORAR)
+    // Buscar / Read
 
-  public void venderBebida(String nombre, int stock) {
+    public List<Bebida> buscarPorNombre(String nombre) {
 
+        ArrayList<Bebida> nombreBebida = new ArrayList<>();
+        for (Bebida bebida : bebidas) {
 
-  }
+            if (bebida.getNombre().equalsIgnoreCase(nombre)) {
 
-  // ObtenerTodas / Delete
+                nombreBebida.add(bebida);
 
-  public List<Bebida> obtenerTodas() {
+            }
+        }
 
-    return bebidas;
+        return nombreBebida;
+    }
 
-  }
+    // Vender / UPDATE
+
+    public void venderBebida(String nombre, int stock) {
+
+        // COLOCARLE A TODO THIS por si acaso
+
+        for (Bebida bebida : this.bebidas) {
+            if (bebida.getNombre().equalsIgnoreCase(nombre)) {
+                if (bebida.getStock() > stock) {
+                    try{
+                        bebida.setStock(bebida.getStock() - stock);
+                        System.out.println("Se vende la bebida: " + bebida.getNombre());
+                    }catch(Exception errorStock){
+                        System.out.println("Error al vender la bebida: " + errorStock.getMessage());
+                    }
+                }
+            }
+        }
+
+    }
+
+    // ObtenerTodas / Delete
+
+    public List<Bebida> obtenerTodas() {
+
+        return bebidas;
+
+    }
 
 }
