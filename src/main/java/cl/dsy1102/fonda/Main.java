01dd9c5a930aa68b2d785d66b0360c6ab1f4a7b9
Fary02 <fa.alam@duocuc.cl>
2026-09-24 18:40:29 -0300

    Se agreaga el ultimo TO DO, el main queda funcional pero con cosas faltantes a incorporar

--- src/main/java/cl/dsy1102/fonda/Main.java
+++ src/main/java/cl/dsy1102/fonda/Main.java
@@ -1,5 +1,7 @@
 package cl.dsy1102.fonda;
 
+import java.util.List;
+
 /**
  * Punto de entrada de la Tarea Fiestas Patrias - Fonda San Belarmino.
  * <p>
@@ -9,41 +11,60 @@ package cl.dsy1102.fonda;
  */
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    // TODO 1: instanciar las cuatro bebidas con los datos del enunciado.
+        // TODO 1: instanciar las cuatro bebidas con los datos del enunciado.
 
-    // Se instancia cada objeto solicitado (Existe otro metodo que segun yo se incorpora mejor)
+        // Se instancia cada objeto solicitado (Existe otro metodo que segun yo se incorpora mejor)
 
-    Bebida chicha = new BebidaAlcoholica("Chicha", 1000, 40, 5000, "una rica chica a base de uva", 2, 12.0, false, true);
-    Bebida piscoSour = new BebidaAlcoholica("Pisco Sour", 500, 25, 2500, "un rico pisco sour", 2, 18.0, true, false);
-    Bebida chichaSinAlcohol = new BebidaSinAlcohol("Chicha sin alchohol", 1000, 60, 1500, "chica en su version para niños", 95);
-    Bebida moteConHuesillo = new BebidaSinAlcohol("Mote con huesillo", 400, 50, 10000, "Lo mejor de la fonda, este mote con huesillo justifica su inversion", 70);
+        Bebida chicha = new BebidaAlcoholica("Chicha", 1000, 40, 5000, "una rica chica a base de uva", 2, 12.0, false, true);
+        Bebida piscoSour = new BebidaAlcoholica("Pisco Sour", 500, 25, 2500, "un rico pisco sour", 2, 18.0, true, false);
+        Bebida chichaSinAlcohol = new BebidaSinAlcohol("Chicha sin alchohol", 1000, 60, 1500, "chica en su version para niños", 95);
+        Bebida moteConHuesillo = new BebidaSinAlcohol("Mote con huesillo", 400, 50, 10000, "Lo mejor de la fonda, este mote con huesillo justifica su inversion", 70);
 
 
-    // TODO 2: marcar la bebida alcoholica 'Chicha' con la venta restringida.
+        // TODO 2: marcar la bebida alcoholica 'Chicha' con la venta restringida.
 
-    // Se castea y restringe segun enunciado, tambien se puede ocupar un bucle for y dentro colocar instanceof
+        // Se castea y restringe segun enunciado, tambien se puede ocupar un bucle for y dentro colocar instanceof
 
-    ((BebidaAlcoholica) chicha).restringirVenta();
+        ((BebidaAlcoholica) chicha).restringirVenta();
 
-    // TODO 3: registrarlas todas en el gestor.
+        // TODO 3: registrarlas todas en el gestor.
 
-    /* FALTA INCORPORAR
+        // Se crea el objeto gestorFonda y se le pasa el objeto de la clase Bebida
 
-    ((GestorFonda) chicha).registrarBebida();
-    ((GestorFonda) piscoSour).registrarBebida();
-    ((GestorFonda) chichaSinAlcohol).registrarBebida();
-    ((GestorFonda) moteConHuesillo).registrarBebida(); */
+        GestorFonda gestorFonda = new GestorFonda();
 
-    // TODO 4: solicitar las cuatro ventas indicadas en el enunciado.
+        gestorFonda.registrarBebida(chicha);
+        gestorFonda.registrarBebida(piscoSour);
+        gestorFonda.registrarBebida(chichaSinAlcohol);
+        gestorFonda.registrarBebida(moteConHuesillo);
 
-    // FALTA INCORPORAR
+        // TODO 4: solicitar las cuatro ventas indicadas en el enunciado.
 
-    // TODO 5: buscar por nombre "Chicha" y listar todas las bebidas.
+        // FALTA logica del SETTER stock
 
-    // FALTA INCORPORAR
+        gestorFonda.venderBebida(chicha.getNombre(),1);
 
-    System.out.println("Proyecto listo. Comienza por la clase Bebida.");
-  }
+        // TODO 5: buscar por nombre "Chicha" y listar todas las bebidas.
+
+        List<Bebida> resultadoBebidas = gestorFonda.buscarPorNombre("Chicha");
+        if (resultadoBebidas.isEmpty()) {
+            System.out.println("La bebida no existe en el catalogo");
+        } else {
+            System.out.println("La bebida existe en el catalogo: ");
+            for (Bebida bebida : resultadoBebidas) {
+                System.out.println(bebida.getNombre());
+            }
+        }
+
+        List<Bebida> obtenerResultado = gestorFonda.obtenerTodas();
+
+        for (Bebida bebida : obtenerResultado) {
+
+            System.out.println("Bebida: " + bebida.getNombre());
+        }
+
+        System.out.println("Proyecto listo. Comienza por la clase Bebida.");
+    }
 }
